5ba6097f6d8dff6df2967d06f6b47150c670443a
François K <francoiska_fr@icloud.com>
2025-04-08 10:32:12 +0200

    Ajout du game asset

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:kafe_app/providers/player_provider.dart';
 import 'package:kafe_app/providers/user_provider.dart';
 import 'package:kafe_app/router/router.dart';
-import 'package:kafe_app/theme.dart';
+import 'package:kafe_app/styles/theme.dart';
 import 'package:provider/provider.dart';
 import 'firebase_options.dart';
 
---
+++ lib/styles/game_asset.dart
@@ -0,0 +1,22 @@
+class GameAsset {
+
+  /// Monnaies
+  static const String deeveeEmoji = "💎";
+  static const String deeveeName = "DeeVee";
+
+  static const String goldGrainEmoji = "🌟";
+  static const String goldGrainName = "Grains d’or";
+
+  /// Plantes & culture
+  static const String FruitEmoji = "🥭"; // Pour le fruit de kafé récolté
+  static const String GrainEmoji = "🥜"; // Pour les grains séchés
+
+  /// Champs & slots
+  static const String fieldEmoji = "🌾"; // Champ de kafé
+  static const String slotEmptyEmoji = "🕳️"; // Slot vide
+  static const String slotPlantEmoji = "🌱"; // Slot en pousse
+  static const String slotReadyEmoji = "🌿"; // Slot prêt à récolter
+
+  /// Types de kafé
+  static const String kafeEmoji = "☕️"; // Général pour un plan de kafé
+}
