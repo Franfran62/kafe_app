1440a1c30919e12635f4fd3a4c394e60e95d2177
François K <francoiska_fr@icloud.com>
2025-04-09 19:46:22 +0200

    meilleur UI pour field_detail

--- lib/screens/field/field_detail_screen.dart
+++ lib/screens/field/field_detail_screen.dart
@@ -24,29 +24,33 @@ class FieldDetailScreen extends StatelessWidget {
       appBar: AppBar(
         leading: BackButton(onPressed: () => Navigator.pop(context)),
         title: Text("Gestion du champs"),
-        actions: const [Icon(Icons.menu)], // TODO later
       ),
       body: SingleChildScrollView(
         padding: const EdgeInsets.all(16),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              field.name,
-              style: Theme.of(context).textTheme.headlineSmall,
-            ),
-            const SizedBox(height: 8),
-            Text("Spécialité : ${field.specialty.label}", style: Theme.of(context).textTheme.titleMedium),
-            const SizedBox(height: 16),
-            ...List.generate(field.slots.length, (i) {
-              final slot = field.slots[i];
-              return SlotItem(
-                index: i,
-                slot: slot,
-                field: field,
-              );
-            }),
-          ],
+        child: ConstrainedBox(
+          constraints:
+              BoxConstraints(minHeight: MediaQuery.of(context).size.height),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                field.name,
+                style: Theme.of(context).textTheme.headlineSmall,
+              ),
+              const SizedBox(height: 8),
+              Text("Spécialité : ${field.specialty.label}",
+                  style: Theme.of(context).textTheme.titleMedium),
+              const SizedBox(height: 16),
+              ...List.generate(field.slots.length, (i) {
+                final slot = field.slots[i];
+                return SlotItem(
+                  index: i,
+                  slot: slot,
+                  field: field,
+                );
+              }),
+            ],
+          ),
         ),
       ),
     );
